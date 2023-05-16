f13d885b22dd8017170ffac35f9f4d3ba0d4cc85
Lyrical168 <ochanda.charles.16@gmail.com>
2023-05-16 11:21:52 +0300

    New stuff

--- app.py
+++ app.py
@@ -96,10 +96,9 @@ def main():
        [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
-
        a,b,c = st.columns([0.2,0.6,0.2])
-with b:
- st.image("banner-picture.jpeg", use_column_width=True)
+       with b:
+          st.image("banner-picture.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
